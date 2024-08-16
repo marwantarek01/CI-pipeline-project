e1b32aa0d532003f1cebe40d6b007756ac6eaeb1
marwantarek01 <marwan.abdeaal2000@gmail.com>
2024-08-16 17:18:39 +0300

    Initial commit

---
+++ app.js
@@ -0,0 +1,18 @@
+const http = require('http');
+
+// Create an HTTP server
+const server = http.createServer((req, res) => {
+    // Set the response header to indicate the content type
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    
+    // Send the response body
+    res.end('Hello, Worllld!  : )\n');
+});
+
+// Define the port the server will listen on
+const port = 3000;
+
+// Start the server and listen on the defined port
+server.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}/`);
+});
